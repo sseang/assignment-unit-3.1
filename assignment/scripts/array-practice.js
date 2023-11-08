95b5e9383683ea3c0c7b0e0232799d056edb5a64
sseang <142060734+sseang@users.noreply.github.com>
2023-11-07 21:37:52 -0600

    Finished required

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -10,8 +10,8 @@ console.log('Animals are: ', animalArray);
 // 1. TODO: Make an array `favoriteFoods` with some favorite foods.
 //    Be sure to console.log your array of foods to the console with
 //    a message, similar to the example above.
-
-
+let favoriteFoods = ['steaks', 'sushi', 'burgers', 'seafood', 'ramen']
+console.log('Foods are: ', favoriteFoods);
 // Array.length: A property that tells you how many items are in a given array.
 console.log('--- 2. Length of an array ---');
 
@@ -22,8 +22,8 @@ console.log('Number of animals:', numberOfAnimals);
 // 2. TODO: Create a variable `numberOfFoods` and use the .length property
 //    to assign it the number value of how many items are inside `favoriteFoods`.
 //    Don't forget to console.log `numberOfFoods` to make sure your code worked!
-
-
+let numberOfFoods = favoriteFoods.length
+console.log('Number of food:', numberOfFoods);
 // Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
@@ -35,14 +35,15 @@ console.log('First animal is', firstAnimal);
 //      of the second item from the `animalArray`.
 //      Be sure to console.log this new variable to make sure your code
 //      worked as expected.
-
-
+let secondAnimal = animalArray[1];
+console.log('Second animal is', secondAnimal);
 // 3.b. TODO: Create a variable `lastAnimal` and assign it the value of
 //      the "last" item in `animalArray`, using its array index.
 //      You'll need to console.log `animalArray` and `lastAnimal` to make
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
-
+let lastAnimal = animalArray[3];
+console.log('Last animal is', lastAnimal)
 // 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length 
 //       property, rather than the exact (hardcoded) index number of the last item.
 
